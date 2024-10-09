42fccfb44864c38c4956722c03e3e17327ad0725
LenYx24 <fokilenard@gmail.com>
2024-10-09 18:03:31 +0200

    added build config in pom.xml so now I can run the jar file

--- src/main/java/com/leny/App.java
+++ src/main/java/com/leny/App.java
@@ -3,9 +3,12 @@ package com.leny;
 import java.util.Scanner;
 
 public class App {
+    public App() {}
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
+        System.out.println(line);
+        sc.close();
     }
 }
