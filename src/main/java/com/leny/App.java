3d75cf7ef1286bfb7c1393e96b9e03faf269ee15
LenYx24 <fokilenard@gmail.com>
2024-10-09 19:05:27 +0200

    added map.jpg, created champ class and tryed to add an image to window

--- src/main/java/com/leny/App.java
+++ src/main/java/com/leny/App.java
@@ -1,12 +1,28 @@
 package com.leny;
 
-import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import javafx.scene.image.Image;
 
 public class App {
+    Champion ch1;
     private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("HelloWorldSwing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File("map.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         //Add the ubiquitous "Hello World" label.
         JLabel label = new JLabel("Hello World");
---
+++ src/main/java/com/leny/Champion.java
@@ -0,0 +1,24 @@
+package com.leny;
+
+import com.sun.javafx.geom.Vec2d;
+
+public class Champion {
+    public Champion(){}
+    public Champion(String n, int g){
+        name = n;
+        gold = g;
+        pos = new Vec2d(10,10);
+    }
+
+    private Vec2d pos;
+    public Vec2d getPos(){return pos;}
+    public void setPos(Vec2d v){pos = v;}
+
+    private String name;
+    public String getName(){return name;}
+    public void setName(String n){name=n;}
+
+    private int gold;
+    public int getGold(){return gold;}
+    public void setGold(int g){gold=g;}
+}
