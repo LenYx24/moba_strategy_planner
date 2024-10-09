3c994272ba08ef9e9ff7385e0cf986dfb65ed54b
LenYx24 <fokilenard@gmail.com>
2024-10-09 16:05:53 +0200

    test: added testing lib deps and made a trivial test which ran successfully

---
+++ src/test/java/com/leny/MainTest.java
@@ -0,0 +1,10 @@
+package com.leny;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void mytest(){
+        assertEquals(1, 1);
+    }
+}
