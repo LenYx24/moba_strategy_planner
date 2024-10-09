07d5fa36a54430f631434550d5202d698f58d9be
LenYx24 <fokilenard@gmail.com>
2024-10-09 19:32:21 +0200

    Now my window is in the center position

--- src/main/java/com/leny/App.java
+++ src/main/java/com/leny/App.java
@@ -1,5 +1,9 @@
 package com.leny;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -8,18 +12,29 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-import javafx.scene.image.Image;
-
 public class App {
-    Champion ch1;
+    private static Point getWindowPosCentered(Dimension windowSize){
+        Point result = new Point();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        result.x = (screenSize.width - windowSize.width) /2;
+        result.y = (screenSize.height - windowSize.height) /2;
+        return result;
+    }
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
+        JFrame frame = new JFrame("Lol Marker");
+        Color c = new Color(100,100,100);
+        frame.setBackground(c);
+        Dimension windowSize = new Dimension(1600,800);
+        frame.setPreferredSize(windowSize);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocation(getWindowPosCentered(windowSize));
+        
 
         BufferedImage img = null;
         try {
-            img = ImageIO.read(new File("map.jpg"));
+            img = ImageIO.read(new File("src/main/resources/map.jpg"));
         } catch (IOException e) {
             e.printStackTrace();
         }
