cc8b7666cd011a15bba29eee5886280a65577f50
LenYx24 <fokilenard@gmail.com>
2024-10-09 21:32:05 +0200

    added to be able to scale image

--- src/main/java/com/leny/App.java
+++ src/main/java/com/leny/App.java
@@ -2,9 +2,13 @@ package com.leny;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -27,8 +31,17 @@ public class App {
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocation(getWindowPosCentered(windowSize));
-        
-        JLabel map = new JLabel(new ImageIcon("src/main/resources/map.jpg"));
+
+        Image mapimage = null;
+        try {
+            mapimage = ImageIO.read(new File("src/main/resources/map.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+        mapimage = mapimage.getScaledInstance(400, 400, Image.SCALE_SMOOTH);
+        ImageIcon mapicon = new ImageIcon(mapimage);
+        JLabel map = new JLabel(mapicon);
         frame.add(map);
 
         //Display the window.
